476926dbba0c7dde7d7ddaa81bc8d0e1b07ae2d0
Jeff <endymionjkb@gmail.com>
2014-12-03 01:17:48 -0500

    Add Brian to admins

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   MAILER_FROM_ADDRESS = 'admin@360showings.com'
-  SHOWINGS_ADMIN = ['endymionjkb@gmail.com']#, 'brian@brianwoodard.com']
+  SHOWINGS_ADMIN = ['endymionjkb@gmail.com', 'brian@brianwoodard.com']
   SMTP_USERNAME = 'lynx@perk.es'
   SMTP_PASSWORD = '^qG2452!'
   EMAIL_REGEX = /^\w.*?@\w.*?\.\w+$/
